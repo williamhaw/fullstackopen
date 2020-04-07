83a22744021d5e1fa5d95d3b54ed753796fe70e1
William Haw <me@williamhaw.com>
2020-04-07 22:47:51 +0800

    Exercise 1.3

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = ({course}) => {
     return (
@@ -19,9 +25,9 @@ const App = () => {
   const Content = () => {
     return (
       <div>
-        <Part part={part1} exercise={exercises1}/>
-        <Part part={part2} exercise={exercises2}/>
-        <Part part={part3} exercise={exercises3}/>
+        <Part part={part1.name} exercise={part1.exercises}/>
+        <Part part={part2.name} exercise={part2.exercises}/>
+        <Part part={part3.name} exercise={part3.exercises}/>
       </div>
     )
   }
@@ -34,7 +40,7 @@ const App = () => {
   
   const Total = () => {
     return(
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     )
   }
 
