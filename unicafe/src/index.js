0be564d4e5b2596c929c1585ce5c68010f7b0cd3
William Haw <me@williamhaw.com>
2020-04-10 12:54:35 +0800

    Exercise 1.9

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -26,17 +26,27 @@ const Button = ({ text, handleClick }) => (
   </button>
 )
 
-const Statistics = ({ good, neutral, bad }) => (
-  <div>
-    <h1>Statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {good + neutral + bad}</p>
-    <p>average {(good - bad) / (good + neutral + bad)}</p>
-    <p>positive {good / (good + neutral + bad) * 100}%</p>
-  </div>
-)
+const Statistics = ({ good, neutral, bad }) => {
+  if (!good && !neutral && !bad)
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive {good / (good + neutral + bad) * 100}%</p>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />,
   document.getElementById('root')
