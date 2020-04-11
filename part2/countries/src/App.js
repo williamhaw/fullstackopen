fdb62a934975206c13620404887a191160d8aa94
William Haw <me@williamhaw.com>
2020-04-11 11:12:44 +0800

    Exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -59,18 +59,37 @@ const Countries = ({ countries }) => {
   }
 }
 
-const CountryDetails = ({ country }) => (
+const CountryDetails = ({ country }) => {
+
+  const [weather, setWeather] = useState({})
+  useEffect(() => {
+    axios
+      .get(encodeURI(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_WEATHER_API_KEY}&query=${country.name}`))
+      .then(response => setWeather(response.data))
+  }, [country])
+
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
+      <h2>Languages</h2>
+      <ul>
+        {country.languages.map(l => <li>{l.name}</li>)}
+      </ul>
+      <img src={country.flag} width='100px' height='100px' />
+      {JSON.stringify(weather) !== '{}' && <Weather countryName={country.name} weather={weather}/>}
+    </div>
+  )
+}
+
+const Weather = ({countryName, weather}) => (
   <div>
-    <h1>{country.name}</h1>
-    <p>capital {country.capital}</p>
-    <p>population {country.population}</p>
-    <h2>Languages</h2>
-    <ul>
-      {country.languages.map(l => <li>{l.name}</li>)}
-    </ul>
-    <img src={country.flag} width='100px' height='100px' />
+    <h2>Weather in {countryName}</h2>
+      <p><b>temperature:</b> {weather.current.temperature}° Celsius</p>
+      <img src={weather.current.weather_icons[0]} />
+      <p><b>wind:</b> {weather.current.wind_speed}mph, direction {weather.current.wind_dir}</p>
   </div>
 )
-
 
 export default App;
