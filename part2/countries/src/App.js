200dbb2179443d24cd6495b1506c3a5901621025
William Haw <me@williamhaw.com>
2020-04-11 10:38:30 +0800

    Exercise 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -21,41 +21,56 @@ const App = () => {
     setCountryName(event.target.value)
   }
 
-  const countrydetails = () => {
-    if (countries.length > 10) {
-      return (
-        <p>Too many matches, specify another filter</p>
-      )
-    } else if (countries.length > 1) {
-      return (
-        <div>
-          {countries.map(c => <p>{c.name}</p>)}
-        </div>
-      )
-    } else if (countries.length === 1) {
-      const country = countries[0]
-      return (
-        <div>
-          <h1>{country.name}</h1>
-          <p>capital {country.capital}</p>
-          <p>population {country.population}</p>
-          <h2>Languages</h2>
-          <ul>
-            {country.languages.map(l => <li>{l.name}</li>)}
-          </ul>
-          <img src={country.flag} width='100px' height='100px'/> 
-        </div>
-      )
-    }
-
-  }
-
   return (
     <div>
       <div>find countries <input value={countryName} onChange={handleCountryName} /></div>
-      {countrydetails()}
+      <Countries countries={countries} />
     </div>
   )
 }
+
+const Countries = ({ countries }) => {
+
+  const [selectedCountry, setSelectedCountry] = useState({})
+
+  const handleSelectedCountry = (country) => {
+    setSelectedCountry(country)
+  }
+
+  if (countries.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  } else if (countries.length > 1) {
+    return (
+      <div>
+        {countries.map(c => <p>{c.name} <button onClick={() => handleSelectedCountry(c)}>show</button></p>)}
+        {JSON.stringify(selectedCountry) !== '{}' && <CountryDetails country={selectedCountry} />}
+      </div>
+    )
+  } else if (countries.length === 1) {
+    const country = countries[0]
+    return (
+      <CountryDetails country={country} />
+    )
+  }
+  else {
+    return ('')
+  }
+}
+
+const CountryDetails = ({ country }) => (
+  <div>
+    <h1>{country.name}</h1>
+    <p>capital {country.capital}</p>
+    <p>population {country.population}</p>
+    <h2>Languages</h2>
+    <ul>
+      {country.languages.map(l => <li>{l.name}</li>)}
+    </ul>
+    <img src={country.flag} width='100px' height='100px' />
+  </div>
+)
+
 
 export default App;
