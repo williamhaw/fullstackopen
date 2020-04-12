fdb9db8eff42850fa21a483f2c7824f70b46ad23
William Haw <me@williamhaw.com>
2020-04-12 16:39:44 +0800

    Exercise 3.2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -32,6 +32,12 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  res.send(`Phonebook has info for ${persons.length} people
+  <p>${new Date()}</p>
+  `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
