e4e6b1ad0a2e6e2d42a6d793a71f7785a8ace804
William Haw <me@williamhaw.com>
2020-04-12 21:01:05 +0800

    Exercise 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+const generateId = () => {
+  return Math.floor(Math.random() * 45987235)
+}
+
+if (process.argv.length < 3) {
+  console.log('Not enough parameters')
+  process.exit(1)
+} else {
+  const password = process.argv[2]
+  const mongoConnectionUrl = `mongodb+srv://phonebook-user:${password}@cluster0-ilhjx.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+  mongoose.connect(mongoConnectionUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+    id: Number
+  })
+
+  const Person = mongoose.model('Person', personSchema)
+
+
+  if (process.argv.length === 3) {
+    //get all entries from db
+    Person.find({}).then(result => {
+      result.forEach(p => console.log(p))
+      mongoose.connection.close()
+    })
+
+  } else if (process.argv.length === 5) {
+    //add new person to MongoDB
+    const newName = process.argv[3]
+    const newNumber = process.argv[4]
+
+    const person = new Person({
+      name: newName,
+      number: newNumber,
+      id: generateId()
+    })
+
+    person.save().then(response => {
+      console.log(`person saved!`)
+      mongoose.connection.close()
+    })
+
+  }
+}
