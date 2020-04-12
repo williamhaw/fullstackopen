2e5514d258f4a59448c0655191452b9f09a65ac9
William Haw <me@williamhaw.com>
2020-04-12 21:58:57 +0800

    Exercise 3.15

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -14,29 +14,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors())
 app.use(express.static('build'))
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
-
 const generateId = () => {
   return Math.floor(Math.random() * 45987235)
 }
@@ -85,7 +62,7 @@ app.get('/api/persons/:id', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
-  persons = persons.filter(p => p.id !== id)
+  Person.findOneAndDelete({ id: id }, (err) => { console.log(err) })
   res.status(204).end()
 })
 
