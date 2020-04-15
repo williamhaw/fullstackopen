c84878f8a5835edd87c0b4a54f59886058dc2467
William Haw <me@williamhaw.com>
2020-04-15 22:53:15 +0800

    Exercise 4.9

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,22 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('', async (request, response) => {
+  const blogs = await Blog.find({})
+  return response.json(blogs)
 })
 
 blogsRouter.post('', (request, response) => {
   const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const result = blog.save()
+  return response.status(201).json(result)
 })
 
 module.exports = blogsRouter
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,21 +1,47 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
+const initialBlogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blog = new Blog(initialBlogs[0])
+  await blog.save()
+  blog = new Blog(initialBlogs[1])
+  await blog.save()
+})
+
+test('Two blogs are returned as JSON when there are two blogs in the database', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  expect(response.body).toHaveLength(2)
+})
+
 test('blogs are returned as JSON array', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.status).toBe(200)
-  expect(response.body).toHaveLength(1)
-  expect(response.body[0]).toEqual({
-    "_id": "5e9580d2b5189811dc752af5",
-    "title": "Go To Statement Considered Harmful",
-    "author": "Edsger W. Dijkstra",
-    "url": "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    "likes": 5,
-    "__v": 0
+  response.body.forEach(blog => {
+    expect(blog.id).toBeDefined()
   })
+
 })
 
 afterAll(() => {
