fb822d30108cebacf20bfc8782be30f7c42e2bb9
William Haw <me@williamhaw.com>
2020-04-25 21:14:59 +0800

    Exercise 4.21

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -17,9 +17,12 @@ blogsRouter.post('', async (request, response, next) => {
   try {
     logger.info(request.body)
     const body = request.body
+    if (!request.token) {
+      return response.status(401).json({ error: "token missing" })
+    }
     const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!request.token || !decodedToken.id) {
-      return response.status(401).json({ error: 'token missing or invalid' })
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
     }
     const user = await User.findById(decodedToken.id)
 
@@ -41,8 +44,22 @@ blogsRouter.post('', async (request, response, next) => {
 
 blogsRouter.delete('/:id', async (request, response, next) => {
   try {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
+    const blog = await Blog.findById(request.params.id)
+    if (!request.token) {
+      return response.status(401).json({ error: "token missing" })
+    }
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    if (blog.user.toString() === user._id.toString()) {
+      await Blog.findByIdAndRemove(request.params.id)
+      response.status(204).end()
+    } else {
+      return response.status(401).json({ error: "blog can only be deleted by user who added it" })
+    }
   } catch (exception) {
     next(exception)
   }
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -28,10 +28,7 @@ const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
     request.token = authorization.substring(7)
-  } else {
-    return response.status(401).json({ error: "authorization token missing" })
   }
-
   next()
 }
 
