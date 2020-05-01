cdf9c9af3fe2f92314b58b144d3c76264915572b
William Haw <me@williamhaw.com>
2020-05-01 19:11:09 +0800

    Exercise 9.3

--- part9/tsapp/exerciseCalculator.ts
+++ part9/tsapp/exerciseCalculator.ts
@@ -12,12 +12,11 @@ const calculateExercises = (
   dailyHours: Array<number>,
   targetHours: number
 ): Result => {
-
-  const average = dailyHours.reduce((a, b) => a + b) / dailyHours.length
+  const average = dailyHours.reduce((a, b) => a + b) / dailyHours.length;
 
   return {
     numDays: dailyHours.length,
-    numTrainingDays: dailyHours.filter(h => h > 0).length,
+    numTrainingDays: dailyHours.filter((h) => h > 0).length,
     originalTargetValue: targetHours,
     averageTime: average,
     isReached: average >= targetHours,
@@ -26,4 +25,31 @@ const calculateExercises = (
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+interface ExerciseValues {
+  dailyHours: Array<number>;
+  targetHours: number;
+}
+
+const parseExerciseArgs = (args: Array<string>): ExerciseValues => {
+  if (args.length < 4)
+    throw new Error(
+      "too few arguments, enter target hours and then hours for each day"
+    );
+  const isAllNumbers =
+    args.slice(2).filter((v) => !isNaN(Number(v))).length === args.length - 2;
+  if (isAllNumbers) {
+    return {
+      dailyHours: args.slice(3).map((v) => Number(v)),
+      targetHours: Number(args[2]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+try {
+  const { dailyHours, targetHours } = parseExerciseArgs(process.argv);
+  console.log(calculateExercises(dailyHours, targetHours));
+} catch (error) {
+  console.log("Error, something bad happened, message: ", error.message);
+}
