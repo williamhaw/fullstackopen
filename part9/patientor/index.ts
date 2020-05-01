f74e440037c349fe9d93d4ae9150941174267a69
William Haw <me@williamhaw.com>
2020-05-01 20:48:01 +0800

    Exercise 9.9

--- part9/patientor/index.ts
+++ part9/patientor/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get("/ping", (_req, res) => {
+app.use(cors());
+
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
